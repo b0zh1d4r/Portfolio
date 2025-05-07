ddebfdfeb7df6fbc49c623f611165c59e02c7d33
Bozhidar Ivanov <bozhidarivanov24@gmail.com>
2025-05-07 15:07:35 +0300

    Add Translation

--- client/src/components/contact/Contact.jsx
+++ client/src/components/contact/Contact.jsx
@@ -1,10 +1,14 @@
+import { useTranslation } from 'react-i18next';
+
 export default function Contact() {
+    const { t } = useTranslation();
+
     return (
         <>
             <section className="contact">
-                <h1>Get in Touch:</h1>
-                <p>I’d love to hear from you! Whether you have a question, a project idea, want to collaborate, or just want to say hello — feel free to reach out. I’m always open to new opportunities and conversations, whether it’s about design, development, or something in between.</p>
-                <p>Let’s build something great together!</p>
+                <h1>{t('contactH1')}</h1>
+                <p>{t('contactP1')}</p>
+                <p>{t('contactP2')}</p>
                 <ul className="links">
                     <li><a href="mailto:bozhidarivanov24@gmail.com"><i className='bx bxl-gmail'></i></a></li>
                     <li><a href="https://linktr.ee/b0zh1d4r" target="_blank"><i className="fa-solid fa-tree"></i></a></li>
--- client/src/components/projects/Projects.jsx
+++ client/src/components/projects/Projects.jsx
@@ -1,12 +1,16 @@
+import { useTranslation } from 'react-i18next';
+
 export default function Projects() {
+    const { t } = useTranslation();
+
     return (
         <>
             <section className="projects">
-                <h1>My Projects:</h1>
-                <p>Each of these projects tells a story. They're more than just lines of code or design elements they reflect my journey as a developer, designer, and problem solver. From the initial spark of an idea to the final product, these projects represent the passion, challenges, and growth that come with every new endeavor.</p>
-                <p>Some of these projects are personal experiments, while others were built in collaboration with clients who trusted me to bring their vision to life. Whether it's a side project, something I’ve built for fun, or a professional piece of work, each project has taught me something new and pushed me to keep improving.</p>
-                <p>Take a look at what I've created, and if you're inspired or curious about working together, I'd love to chat!</p>
-                <p>➤ Link: <a href="https://github.com/b0zh1d4r?tab=repositories" target="_blank">https://github.com/b0zh1d4r?tab=repositories</a></p>
+                <h1>{t('projectsH1')}</h1>
+                <p>{t('projectsP1')}</p>
+                <p>{t('projectsP2')}</p>
+                <p>{t('projectsP3')}</p>
+                <p>➤ {t('projectsP4')} <a href="https://github.com/b0zh1d4r?tab=repositories" target="_blank">https://github.com/b0zh1d4r?tab=repositories</a></p>
             </section>
         </>
     )
